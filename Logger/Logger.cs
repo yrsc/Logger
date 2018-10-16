1fc2c840c51a38f363c745274117e369ad8d4a16
Yero <670363939@qq.com>
2018-10-16 10:30:14 +0800

    +[init]init project

---
+++ Logger/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System;
+
+namespace Framework
+{
+    public class Logger
+    {
+        public enum LoggerType
+        {
+            Unity,
+            Console
+        }
+
+        private static Action<string, string> _InternalLog = null;
+
+        public static void RegisterLogFunc(Action<string, string> logFunc)
+        {
+            _InternalLog = logFunc;
+        }
+
+        public static void UnRegisterLogFunc(Action<string, string> logFunc)
+        {
+            _InternalLog = null;
+        }
+
+        public static void Log(string str)
+        {
+            StackTrace st = new StackTrace(true);
+            StackFrame[] sf = st.GetFrames();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < sf.Length; i++)
+            {
+                sb.AppendLine(string.Format("{0}:{1}()(at {2}:{3})", sf[i].GetMethod().DeclaringType.FullName, sf[i].GetMethod().Name, sf[i].GetFileName(), sf[i].GetFileLineNumber()));
+            }
+            LoggerConsole.Show(str, sb.ToString());
+            if (_InternalLog != null)
+            {
+                _InternalLog(str, sb.ToString());
+            }     
+        }
+    }
+}
+
---
+++ Logger/LoggerConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework
+{
+    public class LoggerConsole
+    {
+#if Unity
+        public static void Show(string str, string stackTrace)
+        {
+            UnityEngine.Debug.Log(str);
+        }
+#else
+        public static void Show(string str, string stackTrace)
+        {
+            Console.WriteLine(str);
+        }
+#endif
+    }
+}
