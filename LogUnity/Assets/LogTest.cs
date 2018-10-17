061fbf51bf0ca0fd2c003d0ec0775cd4692b958e
Yero <670363939@qq.com>
2018-10-17 20:23:03 +0800

    +[log]add log unity project

---
+++ LogUnity/Assets/LogTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+public class LogTest : MonoBehaviour {
+
+    private void Awake()
+    {
+        Framework.Logger.SetMask(new Framework.Logger.LoggerLevel[] { Framework.Logger.LoggerLevel.Error, Framework.Logger.LoggerLevel.Warning});
+        Framework.Logger.Log(Framework.Logger.LoggerLevel.Warning, "Awake");
+    }
+
+    public void Log(string str, string stacktrace)
+    {
+        Debug.Log("str is " + str);
+        Debug.Log("stack trace is " + stacktrace);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Framework.Logger.Log(Framework.Logger.LoggerLevel.Warning, "a");
+        Framework.Logger.Log(Framework.Logger.LoggerLevel.Warning, "ab");
+        Framework.Logger.Log(Framework.Logger.LoggerLevel.Warning, "abc");
+    }
+
+    private int cnt = 0;
+    // Update is called once per frame
+    void Update () {
+        if(Time.frameCount % 10 == 0)
+        {
+            Framework.Logger.Log(Framework.Logger.LoggerLevel.Warning, cnt++.ToString());
+        }
+	}
+}
